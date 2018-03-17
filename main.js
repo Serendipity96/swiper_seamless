4d20c82ebe641ae04edfa0f95de0bf2e16b49011
Serendipityy96 <justserendipity96@gmail.com>
2018-03-17 23:01:29 +0800

    format

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
 let n
 init()
-setInterval(()=>{
-    activeButton(getButton(n))
+setInterval(() => {
+    activeButton(getButton(n)
+)
 makeLeave(getImage(n))
-    .one('transitionend', (e)=>{
-    makeEnter($(e.currentTarget))
+    .one('transitionend', (e) => {
+    makeEnter($(e.currentTarget)
+)
 })
-makeCurrent(getImage(n+1))
-n++
+makeCurrent(getImage(n + 1))
+n++;
 },3000)
 
-function getImage(n){
+function getImage(n) {
     return $(`.images > img:nth-child(${computeN(n)})`)
 }
 
-function computeN(n){
-    if(n>3){
-        n = n%3
-        if (n===0){
-            n =3
+function computeN(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
+            n = 3
         }
     }
     return n //  1 2 3
 }
 
-function init(){
+function init() {
     n = 1
     $(`.images > img:nth-child(${n})`)
         .addClass('current')
@@ -36,22 +38,24 @@ function init(){
         .removeClass('red');
 }
 
-function makeCurrent($node){
+function makeCurrent($node) {
     return $node.removeClass('enter leave').addClass('current')
 }
-function makeLeave($node){
+
+function makeLeave($node) {
     return $node.removeClass('enter current').addClass('leave')
 }
-function makeEnter($node){
+
+function makeEnter($node) {
     return $node.removeClass('leave current').addClass('enter')
 }
 
 //button
-function getButton(n){
-    return $(`#buttons > button:nth-child(${computeN(n+1)})`)
+function getButton(n) {
+    return $(`#buttons > button:nth-child(${computeN(n + 1)})`)
 }
 
-function activeButton($button){
+function activeButton($button) {
     return $button.addClass('red')
         .siblings('.red')
         .removeClass('red');
