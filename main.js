6f5252ccd90d131eabc7cfa333eaad22c7498800
Serendipityy96 <justserendipity96@gmail.com>
2018-03-22 21:10:04 +0800

    format again

--- main.js
+++ main.js
@@ -1,60 +1,56 @@
-let n
-init()
-setInterval(() => {
-    activeButton(getButton(n))
-    makeLeave(getImage(n))
-    .one('transitionend', (e) => {
-    makeEnter($(e.currentTarget))
-    })
-    makeCurrent(getImage(n + 1))
-    n++;
-},3000)
+let $buttons = $('#buttons>li')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
 
-function getImage(n) {
-    return $(`.images > img:nth-child(${computeN(n)})`)
+makeFakeSlides()
+init()
+bindEvents()
+
+function bindEvents() {
+    $buttons.eq(0).on('click', function () {
+        if (current == 4) {
+            $slides.css({transform: 'translateX(-7356px)'})
+                .one('transitionend', function () {
+                    $slides.hide().offset()
+                    $slides.css({transform: 'translateX(-1226px)'}).show()
+                })
+        } else {
+            $slides.css({transform: 'translateX(-1226px)'})
+        }
+        $buttons.eq(0).addClass('select').siblings().removeClass('select')
+        current = 0
+    })
+    $buttons.eq(1).on('click', function () {
+        $slides.css({transform: 'translateX(-2452px)'})
+        $buttons.eq(1).addClass('select').siblings().removeClass('select')
+        current = 1
+    })
+    $buttons.eq(2).on('click', function () {
+        $slides.css({transform: 'translateX(-3678px)'})
+        $buttons.eq(2).addClass('select').siblings().removeClass('select')
+        current = 2
+    })
+    $buttons.eq(3).on('click', function () {
+        $slides.css({transform: 'translateX(-4904px)'})
+        $buttons.eq(3).addClass('select').siblings().removeClass('select')
+        current = 3
+    })
+    $buttons.eq(4).on('click', function () {
+        $slides.css({transform: 'translateX(-6130px)'})
+        $buttons.eq(4).addClass('select').siblings().removeClass('select')
+        current = 4
+    })
 }
 
-function computeN(n) {
-    if (n > 3) {
-        n = n % 3
-        if (n === 0) {
-            n = 3
-        }
-    }
-    return n //  1 2 3
+
+function makeFakeSlides() {
+    let firstCopy = $images.eq(0).clone()
+    let lastCopy = $images.eq($images.length - 1).clone()
+    $slides.append(firstCopy)
+    $slides.prepend(lastCopy)
 }
 
 function init() {
-    n = 1
-    $(`.images > img:nth-child(${n})`)
-        .addClass('current')
-        .siblings()
-        .addClass('enter')
-    $(`#buttons > button:nth-child(${computeN(n)})`)
-        .addClass('red')
-        .siblings('.red')
-        .removeClass('red');
-}
-
-function makeCurrent($node) {
-    return $node.removeClass('enter leave').addClass('current')
-}
-
-function makeLeave($node) {
-    return $node.removeClass('enter current').addClass('leave')
-}
-
-function makeEnter($node) {
-    return $node.removeClass('leave current').addClass('enter')
-}
-
-//button
-function getButton(n) {
-    return $(`#buttons > button:nth-child(${computeN(n + 1)})`)
-}
-
-function activeButton($button) {
-    return $button.addClass('red')
-        .siblings('.red')
-        .removeClass('red');
+    $slides.css({transform: 'translateX(-1226px)'})
 }
