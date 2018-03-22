fa427445e219a185e358868f2a7d14f15f6f8ca5
Serendipityy96 <justserendipity96@gmail.com>
2018-03-22 22:14:44 +0800

    optimize code

--- main.js
+++ main.js
@@ -2,47 +2,53 @@ let $buttons = $('#buttons>li')
 let $slides = $('#slides')
 let $images = $slides.children('img')
 let current = 0
-
+let timer = null;
 makeFakeSlides()
 init()
 bindEvents()
-
+$('#previous').on('click', function () {
+    goToSlide(current - 1)
+})
+$('#next').on('click', function () {
+    goToSlide(current + 1)
+})
+timer = setInterval(function () {
+    goToSlide(current + 1)
+}, 2000)
+$('.window').on('mouseenter',(function () {
+    clearInterval(timer)
+})).on('mouseleave',(function () {
+    timer = setInterval(function () {
+        goToSlide(current + 1)
+    }, 2000);
+}))
 function bindEvents() {
-    $buttons.eq(0).on('click', function () {
-        if (current == 4) {
-            $slides.css({transform: 'translateX(-7356px)'})
-                .one('transitionend', function () {
-                    $slides.hide().offset()
-                    $slides.css({transform: 'translateX(-1226px)'}).show()
-                })
-        } else {
-            $slides.css({transform: 'translateX(-1226px)'})
-        }
-        $buttons.eq(0).addClass('select').siblings().removeClass('select')
-        current = 0
-    })
-    $buttons.eq(1).on('click', function () {
-        $slides.css({transform: 'translateX(-2452px)'})
-        $buttons.eq(1).addClass('select').siblings().removeClass('select')
-        current = 1
-    })
-    $buttons.eq(2).on('click', function () {
-        $slides.css({transform: 'translateX(-3678px)'})
-        $buttons.eq(2).addClass('select').siblings().removeClass('select')
-        current = 2
-    })
-    $buttons.eq(3).on('click', function () {
-        $slides.css({transform: 'translateX(-4904px)'})
-        $buttons.eq(3).addClass('select').siblings().removeClass('select')
-        current = 3
-    })
-    $buttons.eq(4).on('click', function () {
-        $slides.css({transform: 'translateX(-6130px)'})
-        $buttons.eq(4).addClass('select').siblings().removeClass('select')
-        current = 4
+    $('#buttons').on('click', 'li', function (ev) {
+        let $button = $(ev.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
     })
 }
 
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length - 1
+    }
+    if (current === $buttons.length - 1 && index === 0) {
+        $slides.css({transform: `translateX(${-($buttons.length + 1) * 1226}px)`})
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({transform: `translateX(${-(index + 1) * 1226}px)`}).show()
+            })
+    } else {
+        $slides.css({transform: `translateX(${-(index + 1) * 1226}px)`})
+    }
+    $buttons.eq(index).addClass('select').siblings().removeClass('select')
+    current = index
+
+}
 
 function makeFakeSlides() {
     let firstCopy = $images.eq(0).clone()
